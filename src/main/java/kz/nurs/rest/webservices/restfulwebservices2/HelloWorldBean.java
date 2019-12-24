89e2f76f28c75e06f9a7de57bb2c5021b6fae14a
NursultanMuss <nursalga@gmail.com>
2019-12-24 15:26:52 +0600

    13. Step 04

---
+++ src/main/java/kz/nurs/rest/webservices/restfulwebservices2/HelloWorldBean.java
@@ -0,0 +1,24 @@
+package kz.nurs.rest.webservices.restfulwebservices2;
+
+public class HelloWorldBean {
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public HelloWorldBean(String message) {
+        this.message= message;
+    }
+}
