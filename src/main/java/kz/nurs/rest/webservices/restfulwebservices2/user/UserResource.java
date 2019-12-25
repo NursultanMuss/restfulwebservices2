1fd97935e730dac61dad7a8135c334b216146408
NursultanMuss <nursalga@gmail.com>
2019-12-25 10:20:11 +0600

    19. Step 08 не доделал

---
+++ src/main/java/kz/nurs/rest/webservices/restfulwebservices2/user/UserResource.java
@@ -0,0 +1,11 @@
+package kz.nurs.rest.webservices.restfulwebservices2.user;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserResource {
+
+    private UserDaoService
+    //retrieve allUsers
+    //retrieveUser(int id)
+}
