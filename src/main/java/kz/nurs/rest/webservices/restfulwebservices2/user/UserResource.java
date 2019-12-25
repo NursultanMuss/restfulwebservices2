05c0469cce5fbe08d00a88474cad53f8b56b1d91
NursultanMuss <nursalga@gmail.com>
2019-12-25 12:24:24 +0600

    19. Step 08

--- src/main/java/kz/nurs/rest/webservices/restfulwebservices2/user/UserResource.java
+++ src/main/java/kz/nurs/rest/webservices/restfulwebservices2/user/UserResource.java
@@ -1,11 +1,28 @@
 package kz.nurs.rest.webservices.restfulwebservices2.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserResource {
 
-    private UserDaoService
+    @Autowired
+    private UserDaoService service;
     //retrieve allUsers
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers(){
+        return service.findAll();
+    }
+
     //retrieveUser(int id)
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id){
+        return service.findOne(id);
+    }
+
 }
