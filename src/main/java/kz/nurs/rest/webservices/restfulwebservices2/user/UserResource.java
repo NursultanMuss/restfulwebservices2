44c954f3aff8b8f2692d85a2514ca80c038e0268
NursultanMuss <nursalga@gmail.com>
2019-12-26 12:12:44 +0600

    23. Step 12

--- src/main/java/kz/nurs/rest/webservices/restfulwebservices2/user/UserResource.java
+++ src/main/java/kz/nurs/rest/webservices/restfulwebservices2/user/UserResource.java
@@ -1,10 +1,11 @@
 package kz.nurs.rest.webservices.restfulwebservices2.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,7 +23,22 @@ public class UserResource {
     //retrieveUser(int id)
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id){
-        return service.findOne(id);
+        User user = service.findOne(id);
+        if(user == null){
+            throw new UserNotFoundException("id - "+ id);
+        }
+        return user;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<Object> createUser(@RequestBody User user){
+        User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 
 }
