cd6ad08c1b9a6b28d31e715c47f9aa4456a50215
NursultanMuss <nursalga@gmail.com>
2019-12-29 14:49:34 +0600

    40. Step 26

---
+++ src/main/java/kz/nurs/rest/webservices/restfulwebservices2/versioning/PersonVersioningController.java
@@ -0,0 +1,23 @@
+package kz.nurs.rest.webservices.restfulwebservices2.versioning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonVersioningController {
+
+    @GetMapping("v1/person")
+    public PersonV1 personV1(){
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping("v2/person")
+    public PersonV2 personV2(){
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+
+
+
+
+}
